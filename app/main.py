fe41051188ee48f434aec016bdae733f00933b64
A Mo <mail@a-mo.io>
2020-04-08 19:14:19 +0100

    Add 911 functionality

--- app/main.py
+++ app/main.py
@@ -1,30 +1,59 @@
+import yaml
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+DEV_MODE = True
+
+with open('configs/dev.yml' if DEV_MODE else 'configs/prod.yml', 'r') as f:
+    config = yaml.safe_load(f)
+
+# PREFIX = config['prefix']
+
+client = commands.Bot(command_prefix = config['prefix'])
+
+
+# Functionality
+
+@client.command()
+async def setnick(ctx: discord.Message, nickname: str = None):
+    print('setnick called')
+
+    if ctx.author.id == config['ownerId']:
+        print('Attempt to set my nickname to {}'.format(nickname))
+        await ctx.guild.get_member(config['userId']).edit(nick=nickname)
+
+@client.command(name='911')
+async def nineoneone(ctx: discord.Message):
+    print('911 called')
+
+    with open('lib/911.txt', 'r', encoding='utf-8') as f:
+        # print(f.read())
+        await ctx.channel.send(f.read())
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content == '!^' or message.content == '^':
-        # msg = 'Hello {0.author.mention}'.format(message)
+        print('!^ called')
+
         await message.channel.send('I agree!')
 
-    if message.content == '!resetName':
-        print('Attempting to reset my name...')
-        await message.guild.get_member(292810126967046154).edit(nick=None)
+    # if message.content == '!resetName':
+    #     print('Attempting to reset my name...')
+    #     await message.guild.get_member(config['userId']).edit(nick=None)
+
+    await client.process_commands(message)
+
+
+# Initialisation
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print('User: {}'.format(client.user.name))
+    print('Client ID: {}'.format(client.user.id))
     print('------')
 
-# Get token and start bot
-with open('lib/secret.txt', 'r') as f:
-    secret = f.readline()
-
-client.run(secret)
+client.run(config['secret'])
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Hina-chan
+
+Mo's personal Discord bot. She does silly stuff. Not intended for public use.
+
+Version: 2.1.0
