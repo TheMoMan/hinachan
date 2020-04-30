b3142b3f9550a04b40fb944149842e919df7a839
A Mo <mail@a-mo.io>
2020-04-30 23:38:48 +0100

    Add verbose mode to defuse

--- app/handlers/gameUtilHandler.py
+++ app/handlers/gameUtilHandler.py
@@ -1,4 +1,5 @@
 import discord
+import app.utils as utils
 from app.services import gameUtilService
 from discord.ext import commands
 
@@ -9,8 +10,39 @@ class GameUtilHandler(commands.Cog):
 
     @commands.command()
     async def defuse(self, ctx: commands.Context, letters: str=''):
+        flags = utils.getFlags(ctx)
+
         if len(letters) < 2:
             await ctx.channel.send('Need a combination of at least 2 letters!')
+            return
+
+        if '-v' in flags or '--verbose' in flags:
+            display = 30
+
+            wordCategories = self.service.categoriseWordsWithCombination(letters)
+
+            if len(wordCategories) == 0:
+                await ctx.channel.send('No words with combination {} found!'.format(letters.upper()))
+                return
+
+            msg = ''
+
+            for category in wordCategories:
+                line = '**{} - {} letters:**\n`'.format(letters.upper(), len(category[0]))  + '` `'.join(category[0:display]) + '`'
+
+                if len(category) > display:
+                    line += ' and {} more'.format(len(category) - display)
+
+                if len(msg) + len(line) > 1996:
+                    await ctx.channel.send(msg)
+                    msg = line + '\n\n'
+
+                else:
+                    msg += (line + '\n\n')
+
+            await ctx.channel.send(msg)
+
+            return
 
         display = 15
         
@@ -20,7 +52,7 @@ class GameUtilHandler(commands.Cog):
             await ctx.channel.send('No words with combination {} found!'.format(letters.upper()))
 
         else:
-            msg = '{} : `'.format(letters.upper()) + '` `'.join(words[0:display]) + '`'
+            msg = '**{}** : `'.format(letters.upper()) + '` `'.join(words[0:display]) + '`'
 
             if len(words) > display:
                 msg += ' and {} more'.format(len(words) - display)
--- app/services/gameUtilService.py
+++ app/services/gameUtilService.py
@@ -11,3 +11,23 @@ class GameUtilService():
             validWords.sort(key=len)
 
             return validWords
+
+    def categoriseWordsWithCombination(self, letters: str):
+        print('categoriseWordsWithCombination called')
+
+        validWords = self.findWordsWithCombination(letters)
+
+        if len(validWords) == 0:
+            return []
+
+        longest = len(validWords[-1])
+
+        categories = []
+
+        for i in range(longest):
+            category = [word for word in validWords if len(word) == i]
+
+            if len(category) > 0:
+                categories.append(category)
+
+        return categories
